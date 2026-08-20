d9f1a838f96685558ef4bd0f7bf37a02f62426ed
Umanjali Singam <anju.singam2711@gmail.com>
2026-08-20 11:19:02 +0530

    file name changing

---
+++ Shri/loop/6-for-in-loop.js
@@ -0,0 +1,10 @@
+const arr = [10,20,30];
+
+for (const i in arr) {
+   console.log(i);                    //index value = 0,1,2
+   console.log(arr[i]);                //10,20,30
+   
+    
+}
+
+//ctrl+shift+P
